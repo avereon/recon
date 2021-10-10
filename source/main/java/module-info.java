b1cff15f822d986e9f8d17dfdb994cf95647454e
Mark Soderquist <mark@soderquist.net>
2021-10-09 19:27:27 -0600

    Version 1.0

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -8,7 +8,7 @@ module com.avereon.recon {
 	requires com.fasterxml.jackson.core;
 	requires com.fasterxml.jackson.databind;
 	opens com.avereon.recon.bundles;
-	exports com.avereon.recon to com.avereon.xenon, com.avereon.zerra;
+	exports com.avereon.recon to com.avereon.xenon, com.avereon.zarra;
 	provides com.avereon.xenon.Mod with com.avereon.recon.Recon;
 
 }
